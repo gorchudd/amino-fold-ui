31a074e310287007c189809225562af4e5ae66cf
gorch <gorch@TLAPTOP_3>
2025-12-02 02:52:25 -0600

    Finalizing PDB modeling
    Setting up backend.

--- protein-backend/main.py
+++ protein-backend/main.py
@@ -28,10 +28,15 @@ class PredictResponse(BaseModel):
 
 app = FastAPI()
 
+origins = [
+  "http://localhost:5173",
+  "http://127.0.0.1:5173",
+]
+
 # Allow your React dev server to call this
 app.add_middleware(
   CORSMiddleware,
-  allow_origins=["http://localhost:5173", "http://127.0.0.1:5173"],
+  allow_origins=origins,
   allow_credentials=True,
   allow_methods=["*"],
   allow_headers=["*"],
